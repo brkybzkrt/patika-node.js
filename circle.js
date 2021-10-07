a807b3a4471d1217d4fdba712a43e4d26f6dd968
brkybzkrt <b9bozkurt959@gmail.com>
2021-10-07 11:38:04 +0300

    Created odev3

---
+++ circle.js
@@ -0,0 +1,14 @@
+
+const circleArea=(r)=>{
+    let pi=3;
+    const result =pi*(r**2)
+    return result;
+}
+const circleCircumference=(r)=>{
+
+    let pi=3;
+    const result =pi*r*2
+    return result;
+}
+
+module.exports={circleArea,circleCircumference}
